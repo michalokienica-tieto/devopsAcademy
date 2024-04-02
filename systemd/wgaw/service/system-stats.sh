947091c4199ca01c902a3659468de43fae3ee087
W. Gawronski <wojciech@gawronsky.pl>
2024-04-02 09:42:32 +0200

    Fix: font customization. Remove colours etc

--- systemd/wgaw/service/system-stats.sh
+++ systemd/wgaw/service/system-stats.sh
@@ -5,23 +5,19 @@
 # tieto intern           #
 ##########################
 
-BOLD=$(tput bold)
-FONT_NORMAL=$(tput sgr0)
-GRAY='\033[90m'
-RED='\033[0;31m'
 LOG_FILE="/tmp/system-stats.log"
 SLEEP_SECONDS=10
 ACCETABLE_USERS_NUMBER=1
 MEM_WARNING_LIMIT=75
 
 while true; do
-	echo "${BOLD}SYSTEM STATISTICS${FONT_NORMAL}: $(date)" >> "$LOG_FILE"
+	echo "SYSTEM STATISTICS: $(date)" >> "$LOG_FILE"
 
 	#############
 	# CPU  stat #
 	#############
 	cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print $1"%"}')
-	echo "${BOLD}CPU Usage${FONT_NORMAL}:  $cpu_usage | " >> "$LOG_FILE"
+	echo "CPU Usage:  $cpu_usage | " >> "$LOG_FILE"
 	
 	#############
 	# RAM usage #
@@ -31,12 +27,12 @@ while true; do
 	mem_perc=$(free | awk '/Mem/ {printf("%.0f", ($3/$2)*100)}')
 	
 	if [ $mem_perc -gt $MEM_WARNING_LIMIT ];then
-		MEM_MESSAGE="${RED}More than ${MEM_WARNING_LIMIT}% memory usage is used now"
+		MEM_MESSAGE="More than ${MEM_WARNING_LIMIT}% memory usage is used now"
 	else
 		MEM_MESSAGE="Memory usage is normal"
 	fi
 
-	echo "${BOLD}Memory${FONT_NORMAL}:     $mem_perc% | total: ${mem_total} MB, usage: $mem_usage MB | ${BOLD}$MEM_MESSAGE ${FONT_NORMAL}" >> "$LOG_FILE"
+	echo "Memory:     $mem_perc% | total: ${mem_total} MB, usage: $mem_usage MB | $MEM_MESSAGE" >> "$LOG_FILE"
 
 	####################
 	# DISK space usage #
@@ -46,13 +42,13 @@ while true; do
 	disk_used=`echo $disk_data | cut -d ' ' -f2`
 	disk_avaiable=`echo $disk_data | cut -d ' ' -f3`
 	disk_used2=`echo $disk_data | cut -d ' ' -f4`
-	echo "${BOLD}Disk Usage${FONT_NORMAL}: $disk_used2 | total: $disk_total, avaiable: $disk_avaiable " >> "$LOG_FILE"
+	echo "Disk Usage: $disk_used2 | total: $disk_total, avaiable: $disk_avaiable " >> "$LOG_FILE"
 
 	#############
 	# PROCESSES #
 	#############
 	run_sys_proc=`ps -e | wc -l`
-	echo "${GRAY}All running processes: $run_sys_proc${FONT_NORMAL}" >> "$LOG_FILE"
+	echo "All running processes: $run_sys_proc" >> "$LOG_FILE"
 
 	#########
 	# USERS #
@@ -64,7 +60,7 @@ while true; do
 		users_message="Number of logged-in users is normal"
 	fi
 	home_dirs=`ls /home | wc -l`
-	echo "Users: $users, Curr user: $USER. Home directories: $home_dirs | ${BOLD}$users_message${FONT_NORMAL}" >> "$LOG_FILE" 
+	echo "Users: $users, Curr user: $USER. Home directories: $home_dirs | $users_message" >> "$LOG_FILE" 
 	
 	######################
 	# Other instructions #
